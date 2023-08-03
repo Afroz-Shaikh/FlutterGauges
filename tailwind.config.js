a1ea26d3ad806290fbcaa1dfaf9fefdd3d0e63a5
Afroz-Shaikh <afrozshaikh725@gmail.com>
2023-08-03 16:40:08 +0530

    Ui-fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
